86620f5c7f82195c35a29e9694971104e3414a58
mehdi keshavarz <m.keshavarz8231@gmail.com>
2025-01-25 22:00:40 +0330

    feat: create login and register component

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor() {}
 
 }
