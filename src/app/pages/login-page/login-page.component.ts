16f5b193aaa1eaaa3403c50929d951432097f559
mehdi keshavarz <m.keshavarz8231@gmail.com>
2025-02-01 16:20:55 +0330

    first-config: create login template.

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {fakeBackendProvider} from '../../helper/_helper';
+import {JwtInterceptor} from '../../helper/Jwt.Interceptor';
+import {ErrorInterceptor} from '../../helper/error.Interceptor';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatError, MatFormField, MatLabel, MatPrefix} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatInput} from "@angular/material/input";
+import {MyErrorStateMatcher} from '../register-page/register-page.component';
+import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatError,
+    MatFormField,
+    MatIcon,
+    MatInput,
+    MatLabel,
+    MatPrefix,
+    ReactiveFormsModule,
+    RouterLink
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
 
+
+  usernameFormControl = new FormControl('', [Validators.required, Validators.min(6)]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.min(6)]);
+  matcher = new MyErrorStateMatcher();
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, #12192C 0%, rgba(196, 196, 196, 0) 100%)',
+        'custom-gradient-2': 'linear-gradient(180deg, #12192C 0%, rgba(196, 196, 196, 0) 100%)'
+      },
+      colors: {
+        'custom-blue':'#12192C',
+        'custom-gray':'#8590AD'
+      }
+    },
   },
   plugins: [],
 }
