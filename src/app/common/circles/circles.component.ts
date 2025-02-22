1ef399abe70cbec52236845412ee2002d973483a
mehdi keshavarz <m.keshavarz8231@gmail.com>
2025-02-23 01:56:41 +0330

    cheat: creating circles component for styling

---
+++ src/app/common/circles/circles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circles',
+  imports: [],
+  templateUrl: './circles.component.html',
+  styleUrl: './circles.component.scss'
+})
+export class CirclesComponent {
+
+}
