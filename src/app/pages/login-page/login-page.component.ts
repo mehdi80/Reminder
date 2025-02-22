6757e8f52b6c0c906e8c3b08cef90234be06a9eb
mehdi keshavarz <m.keshavarz8231@gmail.com>
2025-02-23 02:08:46 +0330

    cheat: updating login component and decreasing cods

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,40 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatError, MatFormField, MatLabel, MatPrefix} from "@angular/material/form-field";
-import {MatIcon} from "@angular/material/icon";
-import {MatInput} from "@angular/material/input";
 import {MyErrorStateMatcher} from '../register-page/register-page.component';
 import {Router, RouterLink} from '@angular/router';
 import {AuthService} from '../../services/auth-service/auth.service';
 import {SnackbarService} from '../../services/snackbar.service';
+import {SocialLinksComponent} from '../../common/social-links/social-links.component';
+import {CirclesComponent} from '../../common/circles/circles.component';
+import {ButtonComponent} from '../../common/button/button.component';
+import {InputFieldComponent} from "../../common/input-field/input-field.component";
+
 
 @Component({
   selector: 'app-login-page',
   imports: [
     FormsModule,
-    MatError,
-    MatFormField,
-    MatIcon,
-    MatInput,
-    MatLabel,
-    MatPrefix,
     ReactiveFormsModule,
-    RouterLink
+    RouterLink,
+    SocialLinksComponent,
+    CirclesComponent,
+    ButtonComponent,
+    InputFieldComponent,
   ],
   providers: [],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
-  loading:boolean = false;
+  loading: WritableSignal<boolean> = signal(false);
+
+
+  socialOptions = signal([
+    {name: 'Facebook', icon: 'bxl-facebook', link: '#'},
+    {name: 'Google', icon: 'bxl-google', link: '#'},
+    {name: 'Instagram', icon: 'bxl-instagram', link: '#'}
+  ])
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private _snackbar:SnackbarService
-  ) {}
+    private _snackbar: SnackbarService
+  ) {
+  }
 
-  login: FormGroup = new FormGroup({
+  login = new FormGroup({
     username: new FormControl('',
       [
         Validators.required,
@@ -51,26 +59,30 @@ export class LoginPageComponent {
     )
   });
 
-  onSubmit():void {
+  inputFields = [
+    {type: 'text', control: this.login.controls['username'], label: 'Username', icon: 'account_circle'},
+    {type: 'password', control: this.login.controls['password'], label: 'Password', icon: 'lock'},
+  ]
+
+  onSubmit(): void {
     if (this.login.valid) {
-      this.loading = true;
-      const{username, password} = this.login.value;
+      this.loading.set(true);
+      const {username, password} = this.login.value;
       this.authService.login(username, password).subscribe(
         success => {
-          this.loading = false;
+          this.loading.set(false);
           if (success) {
             this.router.navigateByUrl('/dashboard');
-          }else {
+          } else {
             this._snackbar.showMessage('The username or password is incorrect!', 'error')
           }
-        },error => {
-          this.loading = false;
-          console.log(error);
-          this._snackbar.showMessage('Something went wrong!', 'error');
+        }, error => {
+          if (error.error) {
+            this.loading.set(false);
+            this._snackbar.showMessage('Something went wrong!', 'error');
+          }
         }
       )
     }
   }
-
-  matcher = new MyErrorStateMatcher();
 }
